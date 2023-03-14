2473ec427f1bc05803252e3991b32e37f9a1d5a3
srilathamanam <srilatha.manam@gmail.com>
2023-03-14 15:16:31 +0530

    commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ export default () => {
   return (
     <footer className="footer bg-dark text-white mt-5 p-4 text-center">
       Made with <i className="fas fa-heart" /> and <i className="fas fa-code" />{" "}
-      by Soumya Ranjan Mohanty
+      by TecworkTrainee
     </footer>
   );
 };
